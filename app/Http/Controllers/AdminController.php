fef6fa514293f26bebcc1be43b27c12a34fac6db
ezFixBug <tdsstoredau@gmail.com>
2023-11-13 22:14:14 +0700

    get list courses and approve course

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Repositories\AdminUserRepository;
+use App\Constants;
+use App\Repositories\Interfaces\AdminUserRepositoryInterface;
+use App\Repositories\Interfaces\CourseRepositoryInterface;
+use App\Repositories\Interfaces\UserRepositoryInterface;
 use Auth;
 use Illuminate\Http\Request;
 use JWTAuth;
@@ -10,10 +13,15 @@ use JWTAuth;
 class AdminController extends Controller
 {
     private $admin_user_repo;
+    private $user_repo;
+    private $course_repo;
 
-    public function __construct(AdminUserRepository $admin_user_repo)
+
+    public function __construct(AdminUserRepositoryInterface $admin_user_repo, UserRepositoryInterface $user_repo, CourseRepositoryInterface $course_repo)
     {
         $this->admin_user_repo = $admin_user_repo;
+        $this->user_repo = $user_repo;
+        $this->course_repo = $course_repo;
     }
     
     public function login(Request $request)
@@ -49,4 +57,63 @@ class AdminController extends Controller
             'tutors' => $tutors,
         ]);  
     }
+
+    public function approveRequestTutor($user_id)
+    {
+        $user = $this->user_repo->findUserById($user_id);
+
+        if (!$user) {
+            return response()->json([
+                'result' => false,
+                'status' => 404,
+                'message' => "User does not exist"
+            ]);  
+        }
+
+        $user['role_cd'] = Constants::CD_ROLE_TUTOR;
+        $user['status_cd'] = Constants::CD_APPROVED;
+
+        $this->user_repo->updateUser($user);
+
+        return response()->json([
+            'result' => true,
+            'status' => 200,
+        ]);  
+    }
+
+    public function getCourses(Request $request)
+    {
+        $input = $request->all();
+
+        $courses = $this->admin_user_repo->getCourses($input);
+
+        return response()->json([
+            'result' => true,
+            'status' => 200,
+            'courses' => $courses,
+        ]);  
+    }
+
+    public function approveCourse($course_id)
+    {
+        $course = $this->course_repo->getCourseById($course_id);
+
+        if (!$course) {
+            return response()->json([
+                'result' => false,
+                'status' => 404,
+                'message' => "Course does not exist"
+            ]);  
+        }
+
+        $course['status_cd'] = Constants::CD_ACCEPT;
+
+        $this->course_repo->createOrUpdateCourse($course);
+
+        return response()->json([
+            'result' => true,
+            'status' => 200,
+        ]);  
+    }
+
 }
