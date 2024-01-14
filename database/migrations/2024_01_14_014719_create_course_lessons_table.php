e55a0cc055fcf927413dee39c92c7f2fa5f83d63
lyhuynhthi <lyhuynhthi@ncc.asia>
2024-01-14 11:23:09 +0700

    create model lesson course

---
+++ database/migrations/2024_01_14_014719_create_course_lessons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourseLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_lessons', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('url')->nullable();
+            $table->uuid('course_id');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_lessons');
+    }
+}
